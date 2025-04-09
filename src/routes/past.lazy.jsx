4f2abb54774facd5f3e38c16d280f6b313f84ce7
Zhixuan Zhang <zhangzx45@gmail.com>
2025-04-08 21:07:42 -0700

    Add TanStack Query

--- src/routes/past.lazy.jsx
+++ src/routes/past.lazy.jsx
@@ -1,9 +1,54 @@
 import { createLazyFileRoute } from "@tanstack/react-router";
+import { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import getPastOrders from "../api/getPastOrders";
 
 export const Route = createLazyFileRoute("/past")({
-  component: RouteComponent,
+  component: PastOrdersRoute,
 });
 
-function RouteComponent() {
-  return <div>Hello "/past"!</div>;
+function PastOrdersRoute() {
+  const [page, setPage] = useState(1);
+  const { isLoading, data } = useQuery({
+    queryKey: ["past-orders", page],
+    queryFn: () => getPastOrders(page),
+    staleTime: 30000,
+  });
+  if (isLoading) {
+    return (
+      <div className="pass-orders">
+        <h2>Loading...</h2>
+      </div>
+    );
+  }
+  return (
+    <div className="past-orders">
+      <table>
+        <thead>
+          <tr>
+            <td>ID</td>
+            <td>Date</td>
+            <td>Time</td>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((order) => (
+            <tr key={order.order_id}>
+              <td>{order.order_id}</td>
+              <td>{order.date}</td>
+              <td>{order.time}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <div className="pages">
+        <button disabled={page <= 1} onClick={() => setPage(page - 1)}>
+          Previous
+        </button>
+        <button disabled={data.length < 10} onClick={() => setPage(page + 1)}>
+          Next
+        </button>
+      </div>
+    </div>
+  );
 }
